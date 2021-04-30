8b5a90ff8d54997cfacdfe2b63b6939cf25d33bf
E-wave112 <osagieiyayi09@gmail.com>
2021-04-30 21:51:51 +0000

    set up more testing suites

--- test/app.test.js
+++ test/app.test.js
@@ -1,5 +1,24 @@
 //a primer on unit testing
+const app = require("../app");
+const Post = require("../models/product");
+const mongoose = require("mongoose");
+const supertest = require("supertest");
 
 it('Testing to see if Jest works', () => {
     expect(1).toBe(1)
   })
+
+
+  //connect to mongodb before running a test
+beforeEach((done) => {
+  mongoose.connect(process.env.dbURI,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    () => done());
+});
+
+//drop the test data after a test case
+afterEach((done) => {
+  mongoose.connection.db.dropDatabase(() => {
+    mongoose.connection.close(() => done())
+  });
+});
