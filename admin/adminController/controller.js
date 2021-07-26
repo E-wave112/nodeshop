24aa86b21930e497a42356232456e1ca3039f24d
E-wave112 <osagieiyayi09@gmail.com>
2021-07-26 19:20:12 +0000

    added the admin authentication functionality

---
+++ admin/adminController/controller.js
@@ -0,0 +1,88 @@
+const Admin = require('../adminModels/admin')
+const jwt = require('jsonwebtoken');
+
+//handle errors
+const handleErrors = (err)  =>{
+    console.log(err.message, err.code)
+    let errors =  {mail :'', password:''};
+
+    //incorrect email
+    if (err.message === 'incorrect mail'){
+        errors.mail = 'that email is not registered'
+    }
+
+    //incorrect email
+    if (err.message === 'incorrect password'){
+        errors.password = 'that password is incorrect';
+    }
+    //duplicate error code
+    if (err.code === 11000){
+        errors.mail = 'that email is already taken'
+        return errors
+    }
+    //validation errors
+    if (err.message.includes('data validation failed')){
+        Object.values(err.errors).forEach(({properties})=>{
+            errors[properties.path] = properties.message;
+            //console.log(properties)
+        })
+    }
+    return errors
+}
+const maxAge = 3 * 24 * 60 * 60
+const createToken = (id) => {
+    return jwt.sign({id}, process.env.JWT_TOKEN, {
+        expiresIn:maxAge
+    });
+}
+
+module.exports.signup_get = (req, res) => {
+    res.render('signup');
+
+}
+
+module.exports.login_get = (req, res) => {
+    res.render('login');
+    
+}
+
+module.exports.signup_post = async (req, res) => {
+    const {mail, password} = req.body;
+    try{
+       const user = await Admin.create({mail, password});
+       const token = createToken(user._id)
+       res.cookie('jwt',token,{ httpOnly: true ,maxAge: maxAge*1000})
+       res.status(201).json({user:user._id}) 
+    }
+
+    catch (err){
+       const errors =  handleErrors(err)
+       res.status(400).json({errors})
+    }
+    
+}
+
+module.exports.signup_get = (req, res) => {
+    res.render('signup');
+    
+}
+
+module.exports.login_post = async (req, res) => {
+    const { mail, password } = req.body;
+
+    try{
+       const user = await Admin.login(mail,password)
+       const token = createToken(user._id)
+       res.cookie('jwt',token,{ httpOnly: true ,maxAge: maxAge*1000})
+       res.status(200).json({user:user._id})
+    }
+  catch (err){
+      const errors = handleErrors(err)
+      res.status(400).json({errors})
+    }
+}
+
+module.exports.logout_get = (req, res) =>{
+    res.cookie('jwt','',{ httpOnly: true, maxAge:1})
+    res.redirect('/')
+}
---
+++ admin/adminModels/admin.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+
+const adminSchema = new mongoose.Schema({
+    mail:{
+        type:String,
+        required:[true, 'please enter an email address'],
+        unique:true,
+        lowercase:true
+    },
+    password:{
+        type:String,
+        required:[true, 'Please enter password'],
+        minlength:[6,'Minimum password length is six characters']
+    }
+})
+
+//fire a function after doc has been saved to db
+// userSchema.post('save',function (doc, next){
+//     console.log('new user was created & saved', doc)
+//     next();
+// })
+
+
+//fire a function before doc has been saved to db
+adminSchema.pre('save', async function (next){
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt)
+    next();
+})
+
+//static method to login user
+// static method to login user
+adminSchema.statics.login = async function(mail, password) {
+    const user = await this.findOne({ mail });
+    if (user) {
+      const auth = await bcrypt.compare(password, user.password);
+      if (auth) {
+        return user;
+      }
+      throw Error('incorrect password');
+    }
+    throw Error('incorrect mail');
+  };
+
+
+
+const Admin = mongoose.model('admin', adminSchema)
+
+module.exports = Admin
