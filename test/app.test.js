1875b37d98b795c7101f8e6cb34497f8a2558f2e
E-wave112 <osagieiyayi09@gmail.com>
2021-10-09 13:09:39 +0000

    added the authentication middleware

--- test/app.test.js
+++ test/app.test.js
@@ -38,7 +38,7 @@ describe('tests', () => {
     let testId = "605a67748509472e2925dc52"
     let res = await supertest(app).get("/product/" + testId)
     // expect(response.body._id).toBe(product.id);
-    expect(res.header['content-type']).toBe('text/html; charset=utf-8');
+    expect(res.header['content-type']).toBe('text/plain; charset=utf-8');
     expect(res.body).toBeTruthy();
     expect(res.statusCode).toBe(200);
   });
